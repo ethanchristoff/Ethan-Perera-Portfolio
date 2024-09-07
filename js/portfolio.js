d79b24d4a87a0f52634f21eb7d2155f3722589c7
ethanchristoff <93596409+ethanchristoff@users.noreply.github.com>
2024-09-07 18:27:28 +0530

    Updated index.html
    Added a form for users to upload and send emails to me. EmailJS was utilized for this.

--- js/portfolio.js
+++ js/portfolio.js
@@ -18,3 +18,40 @@ function toggleMenu(){
         menuList.style.maxHeight = "0px";
     }
 }
+
+(function(){
+    emailjs.init({
+      publicKey: "3vKfHl_F8eO1j_0RR",
+    });
+ })();
+
+function email_submit() {
+    event.preventDefault();
+
+    // Collect form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('Email').value;
+    const message = document.getElementById('Message').value;
+
+    if (name == '' || email == ''){
+        alert("Ensure that you've entered both you're name and Email address!")
+    }else{
+        const templateParams = {
+            to_email: 'ethan.christoff@gmail.com',
+            from_name: name, 
+            message: message, 
+            reply_to: email 
+        };
+
+        emailjs.send('service_rbgxt4h', 'template_99bnb6g', templateParams)
+            .then(function(response) {
+                alert('Email sent successfully!');
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                alert('Failed to send email. Please try again.');
+                console.log('FAILED...', error);
+            });
+
+        return false; 
+    }
+}
